e25d8e5161cb03effb1b4c669bdbf10802f87ba3
Mário Nunes <mariogar1979@gmail.com>
2014-11-07 16:41:43 -0200

    Corrigido indentação

--- attiny85-blink/blink.c
+++ attiny85-blink/blink.c
@@ -4,14 +4,13 @@
 #define LED_PIN PB4
 int main(void)
 {
-				DDRB |= (1 << LED_PIN);  /* Seta PORTB bit 4 como saida. */
+  DDRB |= (1 << LED_PIN);  /* Seta PORTB bit 4 como saida. */
 
-				while (1) {
-								PORTB |= (1 << LED_PIN);  /* LED on */
-								_delay_ms(5000);
-								PORTB &= ~(1 << LED_PIN); /* LED off */
-								_delay_ms(5000);
-				}
-
-				return 0;
+  while (1) {
+    PORTB |= (1 << LED_PIN);  /* LED on */
+		_delay_ms(5000);
+		PORTB &= ~(1 << LED_PIN); /* LED off */
+		_delay_ms(5000);
+	}
+	return 0;
 }
